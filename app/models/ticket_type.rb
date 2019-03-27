b7cc8696f2ada83e4337cfebb13a4b4b45b9cd90
Maria Pia Sanchez <mapisanchezg@gmail.com>
2019-03-27 14:53:20 -0300

    fix(models): delete ticket_type

--- app/models/ticket_type.rb
+++
@@ -1,5 +0,0 @@
-class TicketType < ApplicationRecord
-  belongs_to :category
-  belongs_to :event
-  has_many :ticket_orders
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ApplicationRecord
-  has_many :Order
+  has_many :orders
 
   def most_expensive_ticket_bought
-    ticket_orders = TicketOrder.find_by(User_id:self.id)
+    ticket_orders = Ticket.find_by(User_id:self.id)
     most_expensive_price = 0
     for ticket in ticket_orders
-      ticket_type = TicketType.find(id:ticket.id)
+      ticket_type = Ticket.find(id:ticket.id)
       most_expensive_price = ticket_type.price if ticket_type.price > most_expensive_price
     end
     most_expensive_price
   end
 
   def most_expensive_ticket_bought_between(start_date, end_date)
-    ticket_orders = TicketOrder.find_by(User_id:self.id)
+    ticket_orders = Ticket.find_by(User_id:self.id)
     most_expensive_price = 0
     for ticket in ticket_orders
-      ticket_type = TicketType.where(id:ticket.id)
+      ticket_type = Ticket.where(id:ticket.id)
       most_expensive_price = ticket_type.price if ticket_type.price > most_expensive_price and ticket.created_at>start_date and ticket.created_at<end_date
     end
     most_expensive_price
