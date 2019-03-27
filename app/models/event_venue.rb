d3d9eefb985b466608e9b870b4ffd1d31e790ac4
Maria Pia Sanchez <mapisanchezg@gmail.com>
2019-03-27 19:38:01 -0300

    feat(models):event_venue's and user's methods created

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -2,6 +2,12 @@ class EventVenue < ApplicationRecord
   has_many :events
 
   def last_attendance
-    return
+      events = Event.where(EventVenue_id:self.id)
+      tickets = Ticket.where(Event_id: events.ids)
+      count = 0
+      for ticket in tickets do
+        count += 1
+      end
+      count
   end
 end
